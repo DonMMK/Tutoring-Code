928e86a36e29c9de5169eb0a2f036b4d7b9abc3a
Don Kaluarachchi <71302996+DonMMK@users.noreply.github.com>
2021-05-06 17:31:34 +1000

    Update globalMin.m

--- Week9/globalMin.m
+++ Week9/globalMin.m
@@ -1,6 +1,8 @@
 %% Thinking process
 % when dy/dx that's your gradient 
 % d2y/dx2 will give you the minimum or maximum at the point
+% A global minimum is the minimum of the entire graph in contrast 
+% a local minimum is the minimum closest from the point being calculated
 % In this exercise we will be using the inbuilt matlab function min
 
 %% Solution 
