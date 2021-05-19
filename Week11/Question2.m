18cc0482aab46fcf68d74914c4005760f6f96f4d
don <misurakalu@gmail.com>
2021-05-19 13:46:18 +1000

    Week 11 Questions done

---
+++ Week11/Question2.m
@@ -0,0 +1,8 @@
+%% Thinking process
+% This question is about computing the riemann sum
+
+%% Solution 
+f = @(x) x.^2;
+a = 0;
+b = 5;
+rsums(f,a,b)
---
+++ Week11/integrate.m
@@ -0,0 +1,11 @@
+%% Thinking process
+% To know how to use the integral function type 
+% doc integral in the command window
+
+%% Solution 
+function Area = integrate(f,a,b)
+    %apply the integral function
+    % the area under the grtaph is the integration across the limits
+    Area = integral(f,a,b);
+    
+end
